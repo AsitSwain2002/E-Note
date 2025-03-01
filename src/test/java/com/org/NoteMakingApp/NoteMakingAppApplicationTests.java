c15562180093fdcf819db79be4c54e1276280528
AsitSwain2002 <asitswain7327@gmil.com>
2025-03-01 20:37:26 +0530

    Add First Project

---
+++ src/test/java/com/org/NoteMakingApp/NoteMakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.NoteMakingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteMakingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
